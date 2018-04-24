ef362c0ac2bb233dbd5bcec7f7653f632b62d3f6
map <1119105394@qq.com>
2018-04-24 16:58:52 +0800

    回显

--- application/admin/controller/Commodity.php
+++ application/admin/controller/Commodity.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace app\admin\controller;
+
 use app\common\controller\Admin;
 use think\Db;
 
@@ -9,150 +11,171 @@ class Commodity extends Admin
     {
         $model = model('WellType');
         $data = $model->select();  //查出井盖类型
-        $this->assign('data',$data);
-        if (request()->isPost()){  //添加井盖信息
+        $this->assign('data', $data);
+        if (request()->isPost()) {  //添加井盖信息
             $mel = model('Well');
             $file = request()->file('img');
-            if($file){
+            if ($file) {
                 $info = $file->move(ROOT_PATH . 'public' . DS . 'uploads');
-                if($info){
+                if ($info) {
                     $sm = input('');
-                    if($sm['well_type_id'] != ''){
+                    if ($sm['well_type_id'] != '') {
                         $sm['file'] = $info->getSaveName();
                         $mel->save($sm);
                         $this->success('发布成功', 'Index/index');
-                    }else{
+                    } else {
                         $error = '类型值不能为空';
-                        $this->assign('error',$error);
+                        $this->assign('error', $error);
                     }
-                }else{
+                } else {
                     echo $file->getError();
                 }
             }
             $sm = input('');
-            if($sm['well_type_id'] != ''){
+            if ($sm['well_type_id'] != '') {
                 $mel->save($sm);
                 $this->success('发布成功', 'Index/index');
-            }else{
+            } else {
                 $error = '类型值不能为空';
-                $this->assign('error',$error);
-            }
-        }
-        return $this->fetch();
-     }
-    public function commodityy(){ //修改井盖信息
-        $model = model('WellType');
-        $data = $model->select();
-        $this->assign('data',$data);
-        $sm = input('');
-        if (request()->isPost()){
-            $mel = model('Well');
-            $file = request()->file('img');
-            if($file){
-                $info = $file->move(ROOT_PATH . 'public' . DS . 'uploads');
-                if($info){
-                    $sm = input('');
-                    if($sm['well_type_id'] != ''){
-                        $sm['file'] = $info->getSaveName();
-                        $mel->where(['id'=>$sm['id']])->update($sm);
-                        $this->success('修改成功', 'Index/index');
-                    }else{
-                        $error = '类型值不能为空';
-                        $this->assign('error',$error);
-                    }
-
-                }else{
-                    echo $file->getError();
-                }
-            }
-            $sm = input('');
-            if($sm['well_type_id'] != ''){
-                $mel->where(['id'=>$sm['id']])->update($sm);
-                $this->success('修改成功', 'Index/index');
-            }else{
-                $error = '类型值不能为空';
-                $this->assign('error',$error);
+                $this->assign('error', $error);
             }
         }
         return $this->fetch();
     }
-    public function lists(){  //查出井盖类型列表
+
+    public function commodityy()
+    { //修改井盖信息
+        $model = model('WellType');
+        $data = $model->select();
+        $this->assign('data', $data);
+        $sm = input('');
+        if (request()->isPost()) {
+            $mel = model('Well');
+            $file = request()->file('img');
+            if ($file) {
+                $info = $file->move(ROOT_PATH . 'public' . DS . 'uploads');
+                if ($info) {
+                    $sm = input('');
+                    if ($sm['well_type_id'] != '') {
+                        $sm['file'] = $info->getSaveName();
+                        $mel->where(['id' => $sm['id']])->update($sm);
+                        $this->success('修改成功', 'Index/index');
+                    } else {
+                        $error = '类型值不能为空';
+                        $this->assign('error', $error);
+                    }
+
+                } else {
+                    echo $file->getError();
+                }
+            }
+            $sm = input('');
+            if ($sm['well_type_id'] != '') {
+                $mel->where(['id' => $sm['id']])->update($sm);
+                $this->success('修改成功', 'Index/index');
+            } else {
+                $error = '类型值不能为空';
+                $this->assign('error', $error);
+            }
+        }
+        return $this->fetch();
+    }
+
+    public function lists()
+    {  //查出井盖类型列表
         $model = model('WellType');
         //$list = $model->paginate(1);
         $list = $model->select();
         //$page = $list->render();
         $this->assign('list', $list);
-       // $this->assign('page', $page);
+        // $this->assign('page', $page);
         return $this->fetch();
     }
-    public function type(){  //添加井盖类型属性值
-        if (request()->isPost()){
+
+    public function type()
+    {  //添加井盖类型属性值
+        if (request()->isPost()) {
             $data = input('');
             $model = model('WellType');
             $validate = validate('WellType');
-            if(!$validate->batch()->check($data)){
+            if (!$validate->batch()->check($data)) {
                 $cw = $validate->getError();
-                $this->assign('cw',$cw);
-            }else{
+                $this->assign('cw', $cw);
+            } else {
                 $model->save($data);
                 $this->success('发布成功', 'Index/index');
             }
         }
-       return $this->fetch();
+        return $this->fetch();
     }
-    public function delete(){ //删除井盖类型相对应的井盖列表也将删除
+
+    public function delete()
+    { //删除井盖类型相对应的井盖列表也将删除
         if (request()->isAjax()) {
-            Db::transaction(function(){
+            Db::transaction(function () {
                 $id = input('id');
-                $sm = Db::table('well_type')->where(['id'=>$id])->find();
+                $sm = Db::table('well_type')->where(['id' => $id])->find();
                 $type_id = $sm['type'];
-                Db::table('well')->where(['well_type_id'=>$type_id])->delete();
-                Db::table('well_type')->where(['id'=>$id])->delete();
+                Db::table('well')->where(['well_type_id' => $type_id])->delete();
+                Db::table('well_type')->where(['id' => $id])->delete();
                 echo 1;
             });
         }
     }
-    public function typee(){  //修改井盖类型
-        if(request()->isPost()){
+
+    public function typee()
+    {  //修改井盖类型
+        if (request()->isPost()) {
             $data = input('');
             $validate = validate('WellType');
-            if(!$validate->batch()->check($data)){
+            if (!$validate->batch()->check($data)) {
                 $cw = $validate->getError();
-                $this->assign('cw',$cw);
-            }else{
+                $this->assign('cw', $cw);
+            } else {
                 $model = model('WellType');
-                $model->where(['id'=>$data['id']])->update($data);
-                $this->success('修改成功', 'Index/index');
+                $model->where(['id' => $data['id']])->update($data);
+                $this->success('修改成功', 'Commodity/lists');
             }
         }
+        //>>回显
+        $id = input('id');
+        $model = model('WellType');
+        $data = $model->where(['id' => $id])->find();
+        $this->assign('data', $data);
         return $this->fetch();
     }
-    public function listss(){ //井盖信息列表
+
+    public function listss()
+    { //井盖信息列表
         $id = input('id');
         $model = model('Well');
-        $list = $model->where(['well_type_id'=>$id])->paginate(8);
+        $list = $model->where(['well_type_id' => $id])->paginate(8);
         //$list = $model->where(['well_type_id'=>$id])->select();
         $page = $list->render();
         $this->assign('list', $list);
         $this->assign('page', $page);
         return $this->fetch();
     }
-    public function delte(){  //删除
-        if(request()->isAjax()){
+
+    public function delte()
+    {  //删除
+        if (request()->isAjax()) {
             $id = input('id');
             $model = model('Well');
-            $model->where(['id'=>$id])->delete();
+            $model->where(['id' => $id])->delete();
             echo 1;
         }
     }
-    public function batch(){ //批量删除
-        if(request()->isAjax()){
-           $data = input('');
-           $model = model('Well');
-           foreach ($data['checkData'] as $value){
-                $model->where(['id'=>$value])->delete();
+
+    public function batch()
+    { //批量删除
+        if (request()->isAjax()) {
+            $data = input('');
+            $model = model('Well');
+            foreach ($data['checkData'] as $value) {
+                $model->where(['id' => $value])->delete();
             }
-           echo 1;
+            echo 1;
         }
     }
 
