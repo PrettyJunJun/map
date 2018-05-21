d462fbfeaa786017c4866214b6a2c075afc0f2ae
sm110 <748536389@qq.com>
2018-05-21 14:03:53 +0800

    1

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,22 +1,22 @@
 <?php
 namespace app\index\controller;
-use app\common\controller;
+
+use app\common\controller\Indexx;
 
 class Index extends Indexx{
     public function index()
     {
-        echo 1;
-        //return $this->fetch();
+        return $this->fetch();
     }
-    /*public function index_ajax(){
+    public function index_ajax(){
     	if(request()->isAjax()){
     		$name = input('name');
     		
     		$data = Db::table('well')->where(['name'=>$name])->find();
     		$sm = json_encode($data);
-    		//echo($sm);
+    		echo($sm);
     		
     		
     	}
-    }*/
+    }
 }
