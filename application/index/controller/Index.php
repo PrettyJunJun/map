05446613ee6fb0362f28e99c3e29db89880ae277
sm110 <748536389@qq.com>
2018-05-21 14:09:48 +0800

    1

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -4,11 +4,13 @@ namespace app\index\controller;
 use app\common\controller\Indexx;
 
 class Index extends Indexx{
+	
     public function index()
     {
         return $this->fetch();
     }
-    public function index_ajax(){
+    public function index_ajax()
+    {
     	if(request()->isAjax()){
     		$name = input('name');
     		$data = Db::table('well')->where(['name'=>$name])->find();
